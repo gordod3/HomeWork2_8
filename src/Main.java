0853c89f6864299d870a6740b04febd5e719b0dc
User <dr.gordod>
2020-02-28 18:47:33 +0300

    V.0.6.1.

--- src/Main.java
+++ src/Main.java
@@ -1,66 +1,83 @@
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Random;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
-    private static String scanner(){
+    private static Random r = new Random();
+    private static HashMap<String, String[]> dictionary = new HashMap();
+
+    private static String[] getArrayString(String[] array){
+        return array;
+    }
+
+    private static void setArrayString(String[] setString, String[] getString){
+        setString = getString;
+    }
+
+    private static String findWordKey(HashMap<String, String[]> map, String word) {
+        Iterator<String> iterator = map.keySet().iterator();
+        while (iterator.hasNext()) {
+            String key = iterator.next();
+            ArrayList<String> arrayList = new ArrayList();
+            Collections.addAll(arrayList, map.get(key));
+            Iterator<String> wordSin = arrayList.iterator();
+            while (wordSin.hasNext()) {
+                String isWord = wordSin.next();
+                if (word.equals(isWord)) {
+                    return key;
+                }
+            }
+        }
+        return null;
+    }
+
+    private static String scanner() {
         Scanner scanner = new Scanner(System.in);
         return scanner.nextLine();
     }
-    //private static Scanner scanner = new Scanner(System.in);
-    private static Random r = new Random();
-    private static boolean isKey(HashMap<String, String[]> map, String word){
-        Iterator iterator = map.keySet().iterator();
-        while (iterator.hasNext()){
-            Object o = iterator.next();
-            if (word.equals(o)){
-                return true;
-            }
-        }
-        return false;
-    }
-    private static void findWordKey(HashMap<String, String[]> map, String[] wordArray){
-        Iterator iterator = map.keySet().iterator();
-        while (iterator.hasNext()){
-            Object o = iterator.next();
-            
-        }
-    }
-    private static void bookWord(HashMap<String, String[]> dictionary){
-        for (;;) {
-            System.out.println("Напишите одно из четырех слов чтобы получить их синоним(Красивый, Вежливый, Богатый, Умный):");
+
+    private static void bookWord(HashMap<String, String[]> dictionary) {
+        for (; ; ) {
+            System.out.println("Напишите слово что-бы получить синоним:");
             String word = scanner();
-            if (isKey(dictionary, word)){
-                try {
+            try {
                 System.out.println(dictionary.get(word)[r.nextInt(dictionary.get(word).length)]);
-            }catch (NullPointerException n){
+            } catch (NullPointerException n) {
                 System.out.println("Такого слова нет или вы допустили ошибку при написание. Попробуйте еще раз.");
             }
-            }else{
-                Iterator<String[]> iterator = dictionary.values().iterator();
-                while (iterator.hasNext()){
-                    String[] o = iterator.next();
-                    for (String i : o) {
-                        if (word.equals(i)){
-
-                        }
-                    }
-                }
-            }
             System.out.println("Хотите повторить? Да/Нет");
-            if (scanner().equals("Нет")){
+            if (scanner().equals("Нет")) {
                 break;
             }
         }
     }
 
     public static void main(String[] args) {
-        HashMap<String, String[]> dictionary = new HashMap();
-        dictionary.put("Красивый", new String[]{"Прекрасный" , "Красочный" , "Привликательный"});
-        dictionary.put("Вежливый", new String[]{"Воспитаный" , "Скромный" , "Манерный"});
-        dictionary.put("Богатый", new String[]{"Состоятельный" , "Щедрый" , "Ценный"});
-        dictionary.put("Умный", new String[]{"Знающий" , "Мудрый" , "Хитрый"});
-        bookWord(dictionary);
+        // Создаю словарь и добовляю туда 4 String слова и каждому ключу привязываю String[] в котором 3 String синонима.
+        dictionary.put("Красивый", new String[]{"Прекрасный", "Красочный", "Привликательный"});
+        dictionary.put("Вежливый", new String[]{"Воспитаный", "Скромный", "Манерный"});
+        dictionary.put("Богатый", new String[]{"Состоятельный", "Щедрый", "Ценный"});
+        dictionary.put("Умный", new String[]{"Знающий", "Мудрый", "Хитрый"});
+        for (int i = 0; i < dictionary.keySet().toArray().length; i++) {
+            Object[] keyArray = dictionary.keySet().toArray();
+            Object[] sinonym = dictionary.values().toArray();
+            for (int j = 0; j < dictionary.get(keyArray[i]).length; j++) {
+                String[] newSinonym = new String[keyArray.length];
+                setArrayString(newSinonym, dictionary.get(findWordKey(dictionary, sinonym[j].toString())));
+                newSinonym[j] = findWordKey(dictionary, sinonym[j].toString());
+                dictionary.put(sinonym[j].toString(), newSinonym);
+            }
+        }
+                bookWord(dictionary);
     }
 }
+
+
+//        Iterator<String[]> iterator = dictionary.values().iterator();
+//        while (iterator.hasNext()){
+//            String[] sinonym = iterator.next();
+//            for (int i = 0; i < sinonym.length; i++) {
+//                String[] newSinonym = new String[3];
+//                setArrayString(newSinonym, sinonym);
+//                newSinonym[i] = findWordKey(dictionary, sinonym[i]);
+//                dictionary.put(sinonym[i], newSinonym);
+//            }
+//        }
